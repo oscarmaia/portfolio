859e158bc99a4e02d0029952e03d4a8b3747d0bc
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-06 00:17:24 -0300

    feat: organize area for content

---
+++ src/components/ContentArea.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export default function ContentArea({ children, id }) {
+  return (
+    <ContainerCentral id={id}>
+      <Container>{children}</Container>
+    </ContainerCentral>
+  );
+}
+
+const ContainerCentral = styled.div`
+  width: 80%;
+  min-height: 100vh;
+  margin: auto;
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 767px) {
+    /* Styles for small mobile devices (up to 767px) */
+    /* Add your small mobile-specific styles here */
+    width: 100%;
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 991px) {
+    /* Styles for medium-sized mobile devices (between 768px and 991px) */
+    /* Add your medium mobile-specific styles here */
+  }
+
+  @media screen and (min-width: 992px) {
+    /* Styles for larger mobile devices (992px and above) */
+    /* Add your large mobile-specific styles here */
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
