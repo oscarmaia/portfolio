7dab5ba6533674ea7cf8d474913eda045ffac82a
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-06 00:13:41 -0300

    feat: dark theme

--- src/resources/GlobalStyle.js
+++ src/resources/GlobalStyle.js
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 import { DarkTheme, LightTheme } from "./Theme";
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style>;
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -28,10 +32,11 @@ footer, header, hgroup, menu, nav, section {
 	display: block;
 }
 body {
+	font-family: 'Roboto', sans-serif;
 	line-height: 1;
     background-color: ${(props) =>
       props.theme === true ? DarkTheme.bg : LightTheme.bg};
-    color: ${(props) => (props.theme === true ? DarkTheme.fg : LightTheme.fg)}
+    color: ${(props) => (props.theme === true ? DarkTheme.fg : LightTheme.fg)};
 }
 ol, ul {
 	list-style: none;
@@ -48,5 +53,22 @@ table {
 	border-collapse: collapse;
 	border-spacing: 0;
 }
+/* Hide the scroll bar for the container */
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 9px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: ${(props) =>
+      props.theme === true ? DarkTheme.detailBg : LightTheme.detailBg};
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: ${(props) =>
+      props.theme === true ? DarkTheme.bg : LightTheme.bg};
+    border-radius: 9px;
+    border: 1px none #ffffff;
+  }
 `;
 export default GlobalStyle;
--- src/resources/Theme.js
+++ src/resources/Theme.js
@@ -1,9 +1,13 @@
 export const LightTheme = {
-    bg:'#fff',
-    fg:'#000'
-}
+  bg: "#F1F6F9",
+  fg: "#212A3E",
+  detailBg: "#9BA4B5",
+  detail2: "#212A3E",
+};
 
 export const DarkTheme = {
-    bg:'#000',
-    fg:'#fff'
-}
+  bg: "#212A3E",
+  fg: "#F1F6F9",
+  detailBg: "#394867",
+  detail2: "#F1F6F9",
+};
