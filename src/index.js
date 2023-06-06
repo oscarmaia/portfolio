bba6b1147263f742adb839d4b54861ee995a1472
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-06 00:14:23 -0300

    refact: remove unused dep.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Hello from "./pages/Hello";
 import GlobalStyle from "./resources/GlobalStyle";
 import PageNotFound from "./pages/PageNotFound";
 import { ThemeProvider, useTheme } from "./context/ThemeContext";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -24,7 +24,7 @@ function App() {
       <GlobalStyle theme={theme} />
       <Header />
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
