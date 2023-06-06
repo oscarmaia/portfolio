a8da6ec0bc3895ac7a0cd7ac8b897c511d0f1b1d
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-06 00:17:05 -0300

    feat: add page not found page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,4 +1,16 @@
-export default function PageNotFound(){
-    
-    return <h1>PageNotFound</h1>
+import styled from "styled-components";
+import ContentArea from "../components/ContentArea";
+
+export default function PageNotFound() {
+  return (
+    <ContentArea>
+      <StyledH1>404 | PAGE NOT FOUND</StyledH1>
+    </ContentArea>
+  );
 }
+
+const StyledH1 = styled.h1`
+  font-size: 4rem;
+  text-align: center;
+`;
+
